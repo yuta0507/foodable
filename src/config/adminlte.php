88991ed4980ab2fb88a8e1d3bc1f8996740180e2
yuta0507 <yutakikkawa0507@gmail.com>
2022-12-29 17:19:45 +0900

    Fix navbar's icon

--- src/config/adminlte.php
+++ src/config/adminlte.php
@@ -312,11 +312,9 @@ return [
             'can'  => 'manage-blog',
         ],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text'        => 'Home',
+            'url'         => 'dashboard',
+            'icon'        => 'fas fa-fw fa-home',
         ],
         ['header' => 'account_settings'],
         [
