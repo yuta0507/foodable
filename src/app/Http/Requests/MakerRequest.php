6993a11c2d832947eb4aa6a9f39f3d5397c36017
yuta0507 <yutakikkawa0507@gmail.com>
2023-01-14 17:23:10 +0900

    Make the maker request

---
+++ src/app/Http/Requests/MakerRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\TakeawayFlag;
+use App\Models\User;
+use App\Rules\ReviewRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class MakerRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'max:255'],
+            'genre' => ['required', 'max:255'],
+            'user_review' => [new ReviewRule],
+            'google_review' => [new ReviewRule],
+            'takeaway_flag' => ['required', Rule::in(array_column(TakeawayFlag::cases(), 'value'))],
+            'url' => ['max:255'],
+        ];
+    }
+}
