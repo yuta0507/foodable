0419294dcac0e75d32221463c555b474fa0bbe29
yuta0507 <yutakikkawa0507@gmail.com>
2023-05-01 22:39:07 +0900

    Use label()

--- src/app/Enums/TakeawayFlag.php
+++ src/app/Enums/TakeawayFlag.php
@@ -7,4 +7,18 @@ enum TakeawayFlag: int
     case Missing = 0;
     case Impossible = 1;
     case Possible = 2;
+
+    /**
+     * For view labels
+     *
+     * @return string
+     */
+    public function label(): string
+    {
+        return match ($this) {
+            self::Missing => "I don't know",
+            self::Impossible => 'Impossible',
+            self::Possible => 'Possible',
+        };
+    }
 }
